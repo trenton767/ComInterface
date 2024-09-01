c31e50cf0498884ebbcb846acfd971f3f9b034be
Travis Renton <travisrenton@ymail.com>
2024-09-01 00:16:22 -0400

    Added Functionality
    Added some functionality to process some request messages.

--- CommsPanel_Interface/Form1.cs
+++ CommsPanel_Interface/Form1.cs
@@ -33,7 +33,8 @@ namespace CommsPanel_Interface
             //Console.WriteLine(await response.Content.ReadAsStringAsync());
             string rsp = await response.Content.ReadAsStringAsync();
             VatDATA = new VATSIM(rsp);
-            debugBox_tb.Text = packageControllers();
+            //debugBox_tb.Text = packageControllers();
+            debugBox_tb.Text = processRequest("$|D1|MMMX_ATIS");
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -60,26 +61,89 @@ namespace CommsPanel_Interface
         private string processRequest(string inRqst)
         {
             string header = "^|PC|";
+            string body = "";
             string[] msgParts = inRqst.Split('|');
+
 
             if (msgParts[0] == "!")
             {
                 //Action
+                if (msgParts.Length >= 4)
+                {
+                    foreach (string obj in msgParts)
+                    {
+                        if (obj.Contains("_STBY"))
+                        {
+                            string freq = obj.Split(',')[1];
+                            if (obj.Contains("COM1"))
+                            {
+                                //FSUIPC Set COM1 Stby to freq
+                            }
+                            else if (obj.Contains("COM2"))
+                            {
+                                //FSUIPC Set COM2 Stby to freq
+                            }
+                            else if (obj.Contains("NAV1"))
+                            {
+                                //FSUIPC Set NAV1 Stby to freq
+                            }
+                            else if (obj.Contains("NAV2"))
+                            {
+                                //FSUIPC Set NAV2 Stby to freq
+                            }
+                        }
+                        if (obj.Contains("_SW"))
+                        {
+                            if (obj.Contains("COM1"))
+                            {
+                                //FSUIPC Switch COM1 Freqs
+                            }
+                            else if (obj.Contains("COM2"))
+                            {
+                                //FSUIPC Switch COM2 Freqs
+                            }
+                            else if (obj.Contains("NAV1"))
+                            {
+                                //FSUIPC Switch NAV1 Freqs
+                            }
+                            else if (obj.Contains("NAV2"))
+                            {
+                                //FSUIPC Switch NAV1 Freqs
+                            }
+                        }
+                        if (obj.Contains("XPDR"))
+                        {
+                            if (obj.Contains("_Code"))
+                            {
+                                string code = obj.Split(',')[1];
+                                //FSUIPC Set XPDR Code
+                            }
+                            else if (obj.Contains("_ALT"))
+                            {
+                                //FSUIPC Set XPDR to ALT/Mode C
+                            }
+                            else if (obj.Contains("_IDENT"))
+                            {
+                                //FSUIPC IDENT
+                            }
+                        }
+                    }
+                }
             }
             else if (msgParts[0] == "#")
             {
-
+                //Connection Request
+                body = body + msgParts[1];
+                //Store Serial Connection
             }
             else if (msgParts[0] == "$")
             {
-
-            }
-            else if (msgParts[0] == "%")
-            {
-
+                //ATIS Request
+                string atisText = VatDATA.atis.First(item => item.callsign == msgParts[2]).text_atis;
             }
 
             return header;
+            
         }
     }
 }
